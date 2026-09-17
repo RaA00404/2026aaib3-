e95d5002609abf119cd311191c5bba22e3ec4bbc
RaA00404 <raven0201a@gmail.com>
2026-09-17 10:21:22 +0800

    Add files via upload

---
+++ week02/week02-2.cpp
@@ -0,0 +1,14 @@
+// week02-2.cpp
+// 使用 命名空間
+// File-Save as .cpp
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "請問你叫什麼名字？";
+    string name;   //宣告字串 name
+    cin >> name;
+    cout << name << "你好，今天教字串";
+}
